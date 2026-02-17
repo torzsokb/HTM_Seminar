c6bb03678000b3c6ea9bf9dea7cd0834729277bf
emruijters <ruijters.eveline@gmail.com>
2026-02-17 15:20:51 +0100

    Add files via upload

--- src/neighborhoods/IntraSwap.java
+++ src/neighborhoods/IntraSwap.java
@@ -10,8 +10,15 @@ import java.util.List;
 public class IntraSwap implements Neighborhood {
     private static final double EPS = 1e-6;
 
+    private double sumL = 0.0;
+    private double sumL2 = 0.0;
+    private double sumC = 0.0;
+    private double sumC2 = 0.0;
+
     @Override
     public List<Move> generateMoves(List<Shift> shifts, RouteCompatibility compatibility) {
+        calculateGlobalSums(shifts);
+
         List<Move> moves = new ArrayList<>();
 
         for (int r = 0; r < shifts.size(); r++) {
@@ -33,8 +40,13 @@ public class IntraSwap implements Neighborhood {
             List<Shift> shifts,
             HTMInstance instance,
             double[][] travelTimes,
-            double maxShiftDuration
+            double maxShiftDuration,
+            ObjectiveFunction objectiveFunction
     ) {
+        Objective.BalancedObj obj = (Objective.BalancedObj) objectiveFunction;
+        double lambdaL = obj.lambdaL;
+        double lambdaC = obj.lambdaC;
+
         Shift s = shifts.get(move.route1);
         List<Integer> ids = s.route;
         int n = ids.size();
@@ -81,14 +93,31 @@ public class IntraSwap implements Neighborhood {
                     travelTimes[nodeI][nextJ];
         }
 
-        double delta = oldCost - newCost;
+        double deltaTravel = oldCost - newCost;
 
-        double newDuration = s.totalTime - delta;
-        if (newDuration > maxShiftDuration) {
+        double oldL = s.totalTime;
+
+        double newL = oldL - deltaTravel;
+        if (newL > maxShiftDuration) {
             return new Evaluation(0, false);
         }
-        if (Math.abs(delta) < EPS) delta = 0.0;
-        return new Evaluation(delta, true);
+
+        double sumLNew  = sumL  - oldL + newL;
+        double sumL2New = sumL2 - oldL * oldL + newL * newL;
+
+        int m = 50;
+        double sseLOld = sumL2 - (sumL * sumL) / m;
+        double sseCOld = sumC2 - (sumC * sumC) / m;
+
+        double sseLNew = sumL2New - (sumLNew * sumLNew) / m;
+
+        double oldObj = sumL + lambdaL * sseLOld + lambdaC * sseCOld;
+        double newObj = sumLNew + lambdaL * sseLNew + lambdaC * sseCOld;
+
+        double improvement = oldObj - newObj;
+
+        if (Math.abs(improvement) < EPS) improvement = 0.0;
+        return new Evaluation(improvement, true);
     }
 
     @Override
@@ -112,5 +141,23 @@ public class IntraSwap implements Neighborhood {
         s.route.set(j, nodeI);
 
         return newShifts;
+    }
+
+    private void calculateGlobalSums(List<Shift> shifts) {
+        sumL = sumL2 = 0.0;
+        sumC = sumC2 = 0.0;
+
+        for (Shift s : shifts) {
+            if (s == null) continue;
+
+            double L = s.totalTime;
+            double C = s.serviceTime;
+
+            sumL  += L;
+            sumL2 += L * L;
+
+            sumC  += C;
+            sumC2 += C * C;
+        }
     }
 }
